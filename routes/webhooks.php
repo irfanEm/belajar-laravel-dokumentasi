e4b2f96509ad7305f867a04a45e4cda270269627
irfanEm <imachmud97@gmail.com>
2024-12-12 17:55:05 +0700

    optional parameter route

--- routes/webhooks.php
+++ routes/webhooks.php
@@ -1,9 +1,36 @@
 <?php
 
+use Illuminate\Http\Request;
 Use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function(){
     return response()->json([
         'pesan' => 'test new config Routing'
+    ]);
+});
+
+// Route parameter
+Route::get('/route-par/{nama}', function(string $nama) {
+    return response()->json([
+        'route-parameter' => $nama
+    ]);
+});
+
+// Route parameter & dependencies injection
+Route::get('/rp-depend/{nama}', function(Request $request, $nama) {
+    return response()->json([
+        'data' => [
+            'request' => $request->header(),
+            'nama' => $nama
+        ],
+    ]);
+});
+
+// Optional route parameter
+Route::get('/opt-param/{nama?}', function (?string $nama = 'fulan') {
+    return response()->json([
+        'data' => [
+            'nama' => $nama
+        ]
     ]);
 });
